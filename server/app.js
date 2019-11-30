a2ba2e770e8a76a21d8f3fbc992f3b9f47d8797f
Nadya Ilinskaya <nilinskaya@seattlecca.org>
2019-11-30 15:21:54 -0800

    fixing multiple lint errors

--- server/app.js
+++ server/app.js
@@ -3,18 +3,18 @@
 const io = require('socket.io')(3000);
 
 io.on('connection', (socket) => {
-    socket.on('username', (data) => {
-        socket.username = data.username;
-        console.log(`${socket.username} joined the chat!`);
+  socket.on('username', (data) => {
+    socket.username = data.username;
+    console.log(`${socket.username} joined the chat!`);
         
-        socket.broadcast.emit('new user', socket.username);
-    });
+    socket.broadcast.emit('new user', socket.username);
+  });
 
-    socket.on('message', (data) => {
-        socket.broadcast.emit('message', {user: socket.username, message: data});
-    });
+  socket.on('message', (data) => {
+    socket.broadcast.emit('message', {user: socket.username, message: data});
+  });
 
-    socket.on('disconnect', () => {
-        console.log(`User left the chat`);
-    });
+  socket.on('disconnect', () => {
+    console.log(`User left the chat`);
+  });
 });
