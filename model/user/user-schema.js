8eca4014acc2e15240b78a3384d4d6a10ed4f721
Martin Balke <Mrtnbalke@gmail.com>
2019-12-02 19:49:26 -0800

    fixing git workflow

--- model/user/user-schema.js
+++ model/user/user-schema.js
@@ -1,12 +1,43 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-  username: {type: String, required: true},
-  password: {type: String, required: true},
+  username: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
-//Needs password authentication
+
+
+// Hashes given password
+userSchema.pre('save', function (next) {
+  bcrypt.hash(this.password, 10)
+    .then(hashedPassword => {
+      this.password = hashedPassword;
+      next();
+    }).catch(error => { throw error; });
+});
+
+// This function compares the password with what's in the schema
+// uses bcrypt to compare the this.password with what's in the Schema
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password)
+    .then(valid => valid ? this : null);
+};
+
+// function that generates a token and assigns it to an _id in the schema
+// uses jsonwebtoken to sign the tokenData and salts it with our .env file's secret
+
+userSchema.methods.generateToken = function () {
+  let tokenData = {
+    id: this._id,
+  };
+  return jwt.sign(tokenData, process.env.SECRET);
+};
+
+//exports user-schema use outside of this file
 
 module.exports = mongoose.model('user', userSchema);
