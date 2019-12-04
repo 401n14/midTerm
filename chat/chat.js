cd87cd286aedef7b6908c3c1976a5a3a88f71937
James Dunn <jamesdu@zillowgroup.com>
2019-12-03 20:15:24 -0800

    Finished functionality for logging user's spoken language and all current chat users

--- chat/chat.js
+++ chat/chat.js
@@ -2,7 +2,7 @@
 
 // Import socket.io client
 const io = require('socket.io-client');
-const socket = io.connect('https://n14-transcribe.herokuapp.com');
+const socket = io.connect('http://localhost:3000');
 
 // Import chalk for terminal styling
 const chalk = require('chalk');
@@ -14,7 +14,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-// Create username for each socket connection
+// Store username and language for each socket connection
 let username;
 
 // Listens for a 'connect' event
@@ -29,25 +29,34 @@ socket.on('connect', () => {
     socket.emit('username', { username });
 
     // Use readline to prompt for language
-    rl.question(chalk.hex('#FF9F1C')('What language do you speak? '), language => {
-      // Emits a 'language' event with user input
-      socket.emit('language', { language });
-    });
+    rl.question(
+      chalk.hex('#FF9F1C')('What language do you speak? '),
+      language => {
+        // Emits a 'language' event with user input
+        socket.emit('language', { language });
+      }
+    );
   });
 });
 
 // When a user sends a message
 // A 'message' event is emitted containing the socket id along with the message entered
 rl.on('line', message => {
-  socket.emit('message', {user: socket.id, message});
+  socket.emit('message', { user: socket.id, message });
 });
 
 // Listens for a 'message' event and console logs data
 socket.on('message', data => {
-  console.log(chalk.hex('#FF9F1C').bold(`${data.user}: `) + `${data.message}`);
+  console.log(chalk.hex('#FF9F1C').bold(`${data.user} (${data.language}): `) + `${data.message}`);
 });
 
 // Listens for a 'new user' event and console logs data
 socket.on('new user', data => {
-  console.log(chalk.hex('#011627').bold(`\n>>>> ${data} joined the chat <<<<\n`));
+  console.log(
+    chalk.hex('#32E875').bold(`\n>>>> ${data} joined the chat <<<<\n`)
+  );
+});
+
+socket.on('list-chat-users', users => {
+  console.log('Current Users: ' + users);
 });
--- server/app.js
+++ server/app.js
@@ -6,8 +6,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const server = express()
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = express().listen(PORT, () =>
+  console.log(`Listening on ${PORT}`)
+);
 
 const io = socketIO(server);
 
@@ -21,43 +22,56 @@ let options = {
 const googleTranslate = require('google-translate')(apiKey, options);
 
 let socketPool = {};
+let userGroup = {};
 
 io.on('connection', socket => {
-  // Listens for 'username' event 
+  // Listens for 'username' event
   socket.on('username', data => {
-    socket.username = data.username;
-    console.log(`${socket.username} joined the chat!`);
+    socket.username = ` ${data.username}`;
+    userGroup[socket.id] = ` ${socket.username}`;
   });
 
   // Listens for 'language' event
   // Calls detect language to determine language user typed in
-  // Adds socket to socket pool with language preference 
+  // Adds socket to socket pool with language preference
   socket.on('language', data => {
     let language;
 
-    googleTranslate.detectLanguage(data.language, function(err, detection){
+    googleTranslate.detectLanguage(data.language, function(err, detection) {
       // if unable to detect a language >> default to english
-      if(!detection) {
+      if (!detection) {
         language = 'en';
       } else {
         language = detection.language;
       }
       socketPool[socket.id] = language;
+      socket.language = socketPool[socket.id];
     });
 
     socket.broadcast.emit('new user', socket.username);
+    // Anytime a new user signs in, console.log all users currently in chat
+    socket.emit('list-chat-users', Object.values(userGroup));
   });
 
   // Listens for 'message' event
-  // Loops through connected sockets in the socket pool 
+  // Loops through connected sockets in the socket pool
   // Translates message according to their language preference and emits 'message' event to socket
   socket.on('message', async data => {
+    // Send user and their spoken language with each message they send
     let user = socket.username;
+    let language = socket.language;
 
     for (let socket in socketPool) {
-      if(socket !== data.user) {
-        googleTranslate.translate(data.message, socketPool[socket], function (err, translation) {
-          io.to(`${socket}`).emit('message', {user: user, message: translation.translatedText});
+      if (socket !== data.user) {
+        googleTranslate.translate(data.message, socketPool[socket], function(
+          err,
+          translation
+        ) {
+          io.to(`${socket}`).emit('message', {
+            user: user,
+            message: translation.translatedText,
+            language,
+          });
         });
       }
     }
