47ea612ef31805addeddf31c8b3950acb41b89c4
morgantatums <morgantatums@zillowgroup.com>
2019-12-03 18:15:51 -0800

    switched url in chat from localhost to heroku

--- chat/chat.js
+++ chat/chat.js
@@ -2,7 +2,7 @@
 
 // Import socket.io client
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:3000/');
+const socket = io.connect('https://n14-transcribe.herokuapp.com');
 
 // Import chalk for terminal styling
 const chalk = require('chalk');
@@ -14,8 +14,8 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-// To store users with their generated color
-let users = {};
+// Create username for each socket connection
+let username;
 
 // Listens for a 'connect' event
 // Prompt for name and language
@@ -23,18 +23,15 @@ socket.on('connect', () => {
   console.log(chalk.hex('#2EC4B6')(`>>>> Welcome to Transcribe <<<<\n`));
 
   // Use readline to prompt for name
-  rl.question(chalk.hex('#FF9F1C')('What is your name? '), username => {
-    socket.username = username;
-    users[socket.username] = getRandomColor();
+  rl.question(chalk.hex('#FF9F1C')('What is your name? '), answer => {
+    username = answer;
     // Emits a 'username' event with user input
     socket.emit('username', { username });
 
     // Use readline to prompt for language
-    rl.question(chalk.hex('#FF9F1C')('Please input your preferred language in your preferred language: '), language => {
+    rl.question(chalk.hex('#FF9F1C')('What language do you speak? '), language => {
       // Emits a 'language' event with user input
       socket.emit('language', { language });
-      console.log(chalk.hex('#2EC4B6')(`\n==== START CHATTING ====\n`));
-
     });
   });
 });
@@ -42,24 +39,15 @@ socket.on('connect', () => {
 // When a user sends a message
 // A 'message' event is emitted containing the socket id along with the message entered
 rl.on('line', message => {
-  let color = users[socket.username];
-  socket.emit('message', {user: socket.id, color: color, message});
+  socket.emit('message', {user: socket.id, message});
 });
 
 // Listens for a 'message' event and console logs data
 socket.on('message', data => {
-  console.log(chalk.hex(`${data.color}`).bold(`${data.user}: `) + `${data.message}`);
+  console.log(chalk.hex('#FF9F1C').bold(`${data.user}: `) + `${data.message}`);
 });
 
 // Listens for a 'new user' event and console logs data
 socket.on('new user', data => {
   console.log(chalk.hex('#011627').bold(`\n>>>> ${data} joined the chat <<<<\n`));
-});
-
-function getRandomColor() {
-  return '#' + parseInt(Math.random() * 0xffffff).toString(16);
-}
-
-socket.on('exit', data => {
-  console.log(chalk.hex('#011627').bold(`\n>>>> ${data} left the chat <<<<\n`));
 });
